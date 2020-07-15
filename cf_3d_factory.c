ba31e7dc7fa59e961000711c407fa22639a766f5
Your Name <you@example.com>
2020-07-15 21:27:40 +0800

    xxx

---
+++ cf_3d_factory.c
@@ -0,0 +1,16 @@
+#include "cf_module/cf_module.h"
+#include "cf_factory/cf_factory.h"
+static void mod_load(void){
+    cf_factory* factory = cf_factory_create("cf_3d_factory");
+    cf_factory_add_factory(factory);
+}
+static void test_mod_deinit_func(void){
+
+}
+static void mod_init(cf_mod* mod){
+    cf_mod_set_name(mod,"cf_3d_factory_mod");
+    cf_mod_set_version(mod,CF_MOD_MK_VERSION(0,0,0));
+    cf_mod_set_load(mod,mod_load);
+}
+
+CF_MOD_INIT(mod_init)
---
+++ demo/main.c
@@ -0,0 +1,17 @@
+#include "cf_module/cf_module.h"
+#include "cf_factory/cf_factory.h"
+#include "cf_thread/cf_thread.h"
+#include <assert.h>
+int main(){
+    cf_mod_setup();
+    cf_factory* factory = cf_find_factory("cf_3d_factory");
+    assert(factory != NULL);
+    cf_element* sdlsink = cf_factory_create_elem(factory,"sdlsink",NULL);
+    assert(sdlsink != NULL);
+
+    cf_element_start(sdlsink);
+    while(true){
+        cf_msleep(10);
+    };
+    return 0;
+}
